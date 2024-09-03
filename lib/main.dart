85c7ae3cc75a97d2b62791ce6f501c176047ae37
Aditya Ladwa <ladwa.aditya@gmail.com>
2024-09-04 00:54:14 +0530

    update env

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_movie/widget/movie_app.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: "keys.env");
+  await dotenv.load(fileName: ".env");
   runApp(MovieApp());
 }
